31c495d88282efbca3b0809d472fa1de1d82c593
iftekar2 <iftekar.hossain123@gmail.com>
2024-12-23 15:29:11 -0500

    First commit for Dinosaur Application

--- DinosaurReposetory/src/main/java/com/DinosaurReposetory/DinosaurReposetory/DinosaurReposetoryApplication.java
+++ DinosaurReposetory/src/main/java/com/DinosaurReposetory/DinosaurReposetory/DinosaurReposetoryApplication.java
@@ -1,13 +1,13 @@
-package com.example.readingList;
+package com.DinosaurReposetory.DinosaurReposetory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReadingListApplication {
+public class DinosaurReposetoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReadingListApplication.class, args);
+		SpringApplication.run(DinosaurReposetoryApplication.class, args);
 	}
 
 }
--- DinosaurReposetory/src/test/java/com/DinosaurReposetory/DinosaurReposetory/DinosaurReposetoryApplicationTests.java
+++ DinosaurReposetory/src/test/java/com/DinosaurReposetory/DinosaurReposetory/DinosaurReposetoryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.readingList;
+package com.DinosaurReposetory.DinosaurReposetory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReadingListApplicationTests {
+class DinosaurReposetoryApplicationTests {
 
 	@Test
 	void contextLoads() {
