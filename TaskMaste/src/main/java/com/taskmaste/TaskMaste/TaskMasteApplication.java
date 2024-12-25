59b4a07c8f25eab23ca27225d9b3f33e46837044
iftekar2 <iftekar.hossain123@gmail.com>
2024-12-24 19:56:46 -0500

    New Project

---
+++ TaskMaste/src/main/java/com/taskmaste/TaskMaste/TaskMasteApplication.java
@@ -0,0 +1,13 @@
+package com.taskmaste.TaskMaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMasteApplication.class, args);
+	}
+
+}
---
+++ TaskMaste/src/test/java/com/taskmaste/TaskMaste/TaskMasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmaste.TaskMaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
