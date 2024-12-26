6fba7c88a6fe9d8bcce248a3ff8066424bc9f017
iftekar2 <iftekar.hossain123@gmail.com>
2024-12-25 21:54:05 -0500

    Implementing LogIn class

---
+++ TaskMaste/src/main/java/com/taskmaste/TaskMaste/Models/LogIn.java
@@ -0,0 +1,19 @@
+package com.taskmaste.TaskMaste.Models;
+
+public class LogIn {
+    private String username;
+    private String password;
+
+    public LogIn(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
