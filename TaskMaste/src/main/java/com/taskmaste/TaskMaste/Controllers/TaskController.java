c31373b17167fe223c0b7bbf4f21c5458d767ef0
iftekar2 <iftekar.hossain123@gmail.com>
2024-12-28 11:28:51 -0500

    Implementing getAllTasks method

--- TaskMaste/src/main/java/com/taskmaste/TaskMaste/Controllers/TaskController.java
+++ TaskMaste/src/main/java/com/taskmaste/TaskMaste/Controllers/TaskController.java
@@ -1,4 +1,28 @@
 package com.taskmaste.TaskMaste.Controllers;
 
+import com.taskmaste.TaskMaste.DAO.interfaces.TaskDao;
+import com.taskmaste.TaskMaste.Models.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    private final TaskDao taskDao;
+
+    @Autowired
+    public TaskController(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+
+    @GetMapping("/{userId}")
+    public List<Task> getAllTasks(@PathVariable int userId){
+        return taskDao.getAllTasks(userId);
+    }
 }
--- TaskMaste/src/main/java/com/taskmaste/TaskMaste/DAO/impl/JdbcTaskDao.java
+++ TaskMaste/src/main/java/com/taskmaste/TaskMaste/DAO/impl/JdbcTaskDao.java
@@ -1,4 +1,96 @@
 package com.taskmaste.TaskMaste.DAO.impl;
 
-public class JdbcTaskDao {
+import com.taskmaste.TaskMaste.DAO.interfaces.TaskDao;
+import com.taskmaste.TaskMaste.Models.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JdbcTaskDao implements TaskDao {
+    private static final Logger log = LoggerFactory.getLogger(JdbcTaskDao.class);
+    private final DataSource dataSource;
+
+    @Autowired
+    public JdbcTaskDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public List<Task> getAllTasks(int userId) {
+        List<Task> tasks = new ArrayList<>();
+        String sql =
+                """
+                SELECT title, description, due_date, completion_status, task_type
+                FROM task_manager.tasks
+                WHERE user_id = ?;
+                """;
+
+        try(
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ){
+            preparedStatement.setInt(1, userId);
+
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                while(resultSet.next()){
+                    tasks.add(new Task(
+                        resultSet.getString("title"),
+                        resultSet.getString("description"),
+                        resultSet.getDate("due_date"),
+                        resultSet.getBoolean("completion_status"),
+                        resultSet.getString("task_type")
+                    ));
+                }
+            }
+        }catch(SQLException e){
+            log.error("Error while trying to get All Tasks");
+        }
+        return tasks;
+    }
+
+    @Override
+    public Task getTaskById(int taskId) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getTasksByType(String taskType, int userId) {
+        return List.of();
+    }
+
+    @Override
+    public boolean createTask(String name, String description, Date dueDate, String taskType, int userId) {
+        return false;
+    }
+
+    @Override
+    public boolean updateTask(int taskId, String name, String description, Date dueDate, String taskType) {
+        return false;
+    }
+
+    @Override
+    public boolean updateTaskStatus(int taskId, boolean completionStatus) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteTask(int taskId) {
+        return false;
+    }
+
+    @Override
+    public boolean isTaskNameAvailable(String name, int userId) {
+        return false;
+    }
 }
